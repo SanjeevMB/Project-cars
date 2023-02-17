ca3cb95d84d60b5c8eb769d8dbe9688076632415
Sanjeev Kumar <sanjeev.kumar.23.2@mountblue.tech>
2023-02-17 15:19:02 +0530

    modify problem5

--- problem5.cjs
+++ problem5.cjs
@@ -2,14 +2,18 @@
 
 /* ===== The car lot manager needs to find out how many cars are older than the year 2000. Using the array you just obtained from the previous problem, find out how many cars were made before the year 2000 and return the array of older cars and log its length. ===== */
 
-function problem5(inventory){
-    let carYear = [];
-    for(let index = 0; index < inventory.length; index++){
-        if(inventory[index].car_year < 2000){
-            carYear.push(inventory[index].car_year)
+function problem5(carsArray){
+    let result = [];
+    if(carsArray.length === 0 || Array.isArray(carsArray) === false){
+        return result;        
+    }else{
+        for(let index = 0; index < carsArray.length; index++){
+            if(carsArray[index].car_year < 2000){
+                result.push(carsArray[index].car_year)
+            }
         }
-    }
-    return carYear;
+    }    
+    return result;
 }
 
 module.exports = problem5;
--- test/testProblem5.cjs
+++ test/testProblem5.cjs
@@ -1,7 +1,7 @@
-const carsArray = require('./inventory.cjs');
+const inventory = require('./inventory.cjs');
 
 let problem5 = require('../problem5.cjs');
 
-let carInfo = problem5(carsArray);
+let carInfo = problem5(inventory);
 
 console.log(carInfo.length);
